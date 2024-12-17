f723bdb7bd9015883bd52d9010078e20bfecd4e6
MrRevillod <lrevillod2022@alu.uct.cl>
2024-12-17 17:28:27 -0300

    refactor: :zap: pm2 save and zoom styles

--- apps/web/src/layouts/AppLayout.tsx
+++ apps/web/src/layouts/AppLayout.tsx
@@ -4,27 +4,9 @@ import Header from "../components/ui/Header"
 import { Show } from "../components/ui/Show"
 import { useAuth } from "../context/AuthContext"
 import { Loading } from "../components/Loading"
-import { useEffect } from "react"
 
 const AppLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-
-	const normalizeScale = () => {
-		const scale = window.devicePixelRatio
-		document.body.style.zoom = `${1 / scale}`
-	}
-
 	const { isAuthenticated, user, loading } = useAuth()
-
-	useEffect(() => {
-
-		normalizeScale()
-		window.addEventListener('resize', normalizeScale)
-
-		return () => {
-			window.removeEventListener('resize', normalizeScale)
-		}
-
-	}, [])
 
 	return (
 		<article className="min-h-screen w-screen bg-gray-50 dark:bg-primary-darker">
