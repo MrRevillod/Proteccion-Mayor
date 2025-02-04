3e25a6f9b88f4a0c3e57c2ddedad536c5928b534
MrRevillod <lrevillod2022@alu.uct.cl>
2025-02-04 13:29:22 -0300

    fix: :art: Se solucionaron errores relacionados formularios

--- apps/dashboard/src/seniors/controllers.ts
+++ apps/dashboard/src/seniors/controllers.ts
@@ -14,9 +14,7 @@ export class SeniorController {
 	) { }
 
 	/**
-	 * Controlador para obtener a 
-	 
-	las personas mayores registradas en la base de datos
+	 * Controlador para obtener a las personas mayores registradas en la base de datos
 	 * según los filtros ingresados en la aplicación web
 	 *
 	 * filtra por id, nombre, email y si está validado
